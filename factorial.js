3649af80ed6840bad92aa173e96f496deb9a8977
Mohammed Ali Shaik <69355089+niharali@users.noreply.github.com>
2023-12-28 06:48:38 +0000

    updated

--- factorial.js
+++ factorial.js
@@ -19,8 +19,33 @@ function calculateFactorial(number) {
 
     console.log(`The factorial of ${number} is ${factorial}.`);
 }
+function sum(number) {
+    // Check if the number is negative
+    if (number < 0) {
+        console.error("Error: Factorial is not defined for negative numbers.");
+        return;
+    }
+
+    // Check if the number is a floating-point number
+    if (number % 1 !== 0) {
+        console.error("Error: Factorial is not defined for floating-point numbers.");
+        return;
+    }
+
+    // Calculate the Sum
+    let sem = 1;
+    for (let i = 2; i <= number; i++) {
+        sum += i;
+    }
+
+    console.log(`The factorial of ${number} is ${factorial}.`);
+}
 
 // Example usage
 calculateFactorial(5); // Output: The factorial of 5 is 120.
 calculateFactorial(-2); // Output: Error: Factorial is not defined for negative numbers.
 calculateFactorial(3.5); // Output: Error: Factorial is not defined for floating-point numbers.
+sum(5); // Output: The factorial of 5 is 120.
+sum(-2); // Output: Error: Factorial is not defined for negative numbers.
+sum(3.5); // Output: Error: Factorial is not defined for floating-point numbers.
+
